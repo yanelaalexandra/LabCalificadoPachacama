24dafa28a62777b9848c914779d4340ba4bb3dc5
yanelaalexandra <yanela.pachacama@tecsup.edu.pe>
2018-04-08 23:34:31 -0500

    Commit en casa

--- app/src/main/java/com/pachacama/examen/labcalificado_pachacama/activities/DetailActivity.java
+++ app/src/main/java/com/pachacama/examen/labcalificado_pachacama/activities/DetailActivity.java
@@ -1,8 +1,16 @@
 package com.pachacama.examen.labcalificado_pachacama.activities;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.pachacama.examen.labcalificado_pachacama.R;
 
@@ -13,6 +21,9 @@ public class DetailActivity extends AppCompatActivity {
 
     private TextView info;
 
+    private String numbercall;
+
+    private String urlw;
 
 
     @Override
@@ -27,5 +38,62 @@ public class DetailActivity extends AppCompatActivity {
         name.setText(this.getIntent().getExtras().getString("name"));
         info.setText(this.getIntent().getExtras().getString("info"));
 
+        numbercall=this.getIntent().getExtras().getString("phone");
+        urlw=this.getIntent().getExtras().getString("url");
+
+
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults){
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST: {
+                if (permissions[0].equals(Manifest.permission.CALL_PHONE)) {
+                    if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                        openCallApplication();
+                    }else{
+                        Toast.makeText(this, "CALL_PHONE permissions declined!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
+    }
+
+
+    private static final int PERMISSIONS_REQUEST = 100;
+
+    public void call(View view){
+
+        // Check permission (Api 22 check in Manifest, Api 23 check by requestPermissions)
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // Dont have permission => request one or many permissions (String[])
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, PERMISSIONS_REQUEST);
+        }else {
+            // Have permission
+            openCallApplication();
+        }
+    }
+
+    public void openCallApplication(){
+        String phoneNumber = numbercall;
+        if(phoneNumber.isEmpty()){
+            Toast.makeText(this, "Phone number required!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:"+phoneNumber));
+
+        // Is necesary to check permission again before startActivity
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED)
+            startActivity(intent);
+    }
+
+    public void openWebsite(View view){
+        String urlweb = urlw;
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("url:"+urlweb));
+        startActivity(intent);
+    }
+
+
 }
--- app/src/main/java/com/pachacama/examen/labcalificado_pachacama/repositorio/BuscadorRepository.java
+++ app/src/main/java/com/pachacama/examen/labcalificado_pachacama/repositorio/BuscadorRepository.java
@@ -9,18 +9,18 @@ public class BuscadorRepository {
     private static List<Buscador> buscadores = new ArrayList<>();
 
     static {
-        buscadores.add(new Buscador( 100, "Jovenes", "Tecsup", "Santa Anita", "7532649", "tecsup.edu.pe", "http://tecsup.com","img","Intituto Privado"));
-        buscadores.add(new Buscador( 200, "Comida", "KFC", "Lima", "8524695", "kfc@gmail.com", "http://kfc.com","img","Restaurant de Comida rápida "));
+        buscadores.add(new Buscador( 100, "Jovenes", "Tecsup", "Santa Anita", "7532649", "tecsup.edu.pe", "http://www.tecsup.edu.pe/home/","img","Intituto Privado"));
+        buscadores.add(new Buscador( 200, "Comida", "KFC", "Lima", "8524695", "kfc@gmail.com", "https://www.kfc.com.pe/Online","img","Restaurant de Comida rápida "));
         buscadores.add(new Buscador( 300, "Deporte", "Cantolao", "Chorrillos", "2583451", "cantolao@gmail.com", "hhtp://cantolao.com","img","Academia de futbol"));
-        buscadores.add(new Buscador( 400, "Maquillaje", "Shepora", "Lima", "2034867", "shepora.@gmail.com", "hhhtp://sheporaperu.com","img","Tienda de Maquillaje"));
-        buscadores.add(new Buscador( 500, "Television", "Samsung", "Miraflores", "7534621", "samsung@gmail.com", "http://samsung","img","Tienda de televisores"));
-        buscadores.add(new Buscador( 600, "Mascotas", "PanchoCavero", "Barranco", "85213647", "caveterinaria@gmail.com", "http://caveropancho.com","img","Veterinaria"));
-        buscadores.add(new Buscador( 700, "Radio", "OndaCero", "Surco", "7534201", "ondacero@gmail.com", "http://ondacero.com","img","Radio juvenil local"));
-        buscadores.add(new Buscador( 800, "Educacion", "I. E. Santa Rosa", "Chorrillos", "1234567", "santarosa@gmail.com", "http://santarosa.com","img","Colegio Inicial-Primaria"));
-        buscadores.add(new Buscador( 900, "Ninhos", "ConeyPark", "Surco", "7531598", "conypark@gmail.com", "http://coney.com","img","Lugar de diversiones"));
-        buscadores.add(new Buscador( 110, "Madres", "Tottus", "SJM", "2648951", "tottus@gmail.com", "http://tottus.com","img","Supermercado"));
+            buscadores.add(new Buscador( 400, "Maquillaje", "Shepora", "Lima", "2034867", "shepora.@gmail.com", "http://www.sephora.es/","img","Tienda de Maquillaje"));
+        buscadores.add(new Buscador( 500, "Television", "Samsung", "Miraflores", "7534621", "samsung@gmail.com", "http://www.samsung.com/pe/","img","Tienda de televisores"));
+        buscadores.add(new Buscador( 600, "Mascotas", "PanchoCavero", "Barranco", "85213647", "caveterinaria@gmail.com", "http://www.panchocavero.com/","img","Veterinaria"));
+        buscadores.add(new Buscador( 700, "Radio", "OndaCero", "Surco", "7534201", "ondacero@gmail.com", "http://www.ondacero.com.pe/","img","Radio juvenil local"));
+        buscadores.add(new Buscador( 800, "Educacion", "I. E. Santa Rosa de Lima", "Chorrillos", "1234567", "santarosa@gmail.com", "http://www.santarosadelima.edu.pe/","img","Colegio Inicial-Primaria"));
+        buscadores.add(new Buscador( 900, "Ninhos", "ConeyPark", "Surco", "7531598", "conypark@gmail.com", "https://coneypark.pe/","img","Lugar de diversiones"));
+        buscadores.add(new Buscador( 110, "Madres", "Tottus", "SJM", "2648951", "tottus@gmail.com", "http://www.tottus.com.pe/tottus/","img","Supermercado"));
 
-    }
+}
 
     public static List<Buscador> getList(){
         return buscadores;
